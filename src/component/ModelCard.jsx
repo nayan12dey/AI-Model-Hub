46d35cb2ce5ee5461d1163dd06f510a38d5d8e91
Nayan Dey <ndeynayan123@gmail.com>
2026-03-29 11:31:01 +0530

    move model rendering logic to reusable ModelCard component

---
+++ src/component/ModelCard.jsx
@@ -0,0 +1,31 @@
+
+import React, { useState } from 'react';
+
+const ModelCard = ({ model }) => {
+    // console.log(title, description, price)
+
+     const [isSubscribed, setIsSubscribed] = useState(false);
+
+     const handleSubscription = () => {
+        setIsSubscribed(true)
+     }
+
+    return (
+        <div className='shadow-lg rounded-lg border p-5 overflow-hidden border-zinc-200'>
+            <div className='flex justify-center h-56 bg-zinc-200 p-4 items-center rounded-xl'>
+                <img className='h-40 w-40 object-contain' src={model.image} alt="" />
+            </div>
+
+            <div className='space-y-2 mt-2'>
+                <h2 className='text-2xl font-bold'>{model.title}</h2>
+                <p>{model.description}</p>
+                <div className='text-2xl font-bold'>${model.price}/month</div>
+                <button onClick={handleSubscription} className='btn bg-red-500 w-full text-white rounded-lg mt-5'>{isSubscribed ? "Subscibed" : "Subscribe Now"}</button>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default ModelCard;
--- src/component/Models.jsx
+++ src/component/Models.jsx
@@ -1,10 +1,12 @@
 
 import React, { use } from 'react';
+import ModelCard from './ModelCard';
 
 const Models = ({ modelPromise }) => {
 
     const models = use(modelPromise);
-    console.log(models);
+    
+   
 
 
 
@@ -17,20 +19,7 @@ const Models = ({ modelPromise }) => {
 
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 p-5 mt-10 max-w-7xl mx-auto'>
                 {
-                    models.map(model => <div className='shadow-lg rounded-lg border p-5 overflow-hidden border-zinc-200'>
-                        <div className='flex justify-center h-56 bg-zinc-200 p-4 items-center rounded-xl'>
-                            <img className='h-40 w-40 object-contain' src={model.image} alt="" />
-                        </div>
-
-                        <div className='space-y-2 mt-2'>
-                            <h2 className='text-2xl font-bold'>{model.title}</h2>
-                            <p>{model.description}</p>
-                            <div className='text-2xl font-bold'>${model.price}/month</div>
-                            <button className='btn bg-red-500 w-full text-white rounded-lg mt-5'>Subscribe Now</button>
-                        </div>
-
-
-                    </div>)
+                    models.map(model => <ModelCard key={model.id} model={model}></ModelCard>)
                 }
             </div>
         </div>
